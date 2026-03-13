1798158d9c77249a50e0f1a73ad6d3e1d5fe3585
Sebastián Barreto <luscorreab@udistrital.edu.co>
2026-03-13 17:15:21 -0500

    iniciando el proyecto

---
+++ src/pa/peleadesumos/Cliente/Control/CLauncher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pa.peleadesumos.Cliente.Control;
+
+/**
+ *
+ * @author Asus
+ */
+public class CLauncher {
+    
+    public static void main(String[] args) {
+        new CControlPrincipal();
+    }
+    
+}
---
+++ src/pa/peleadesumos/Servidor/Control/SLauncher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pa.peleadesumos.Servidor.Control;
+
+/**
+ *
+ * @author Asus
+ */
+public class SLauncher {
+    
+    public static void main(String[] args) {
+        new SControlPrincipal();
+    }
+    
+}
